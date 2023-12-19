ddd5d91ed5758e0ff982839dc2d37eb45d45ca38
Tarde Cetafaju <Tarde.Cetafaju@labfies.org.br>
2023-12-19 17:04:13 -0300

    Almoxarifado

--- assets/scripts/dados.js
+++ assets/scripts/dados.js
@@ -34,18 +34,56 @@ const motivos=[
 const produtos=[
     {
         "idProduto": 1,
-        "Descricao": "banana",
+        "Descricao": "Uva",
         "Estoque": 10,
-        "EstoqueMinimo": 5,
-        "Unidade":'Pç',
-        "Valor": 15,
-    },
-    {
-        "idProduto": 2,
-        "Descricao": "uva",
-        "Estoque": 5,
         "EstoqueMinimo": 5,
         "Unidade":'Pç',
         "Valor": 10,
     },
+    {
+        "idProduto": 2,
+        "Descricao": "Morando",
+        "Estoque": 5,
+        "EstoqueMinimo": 5,
+        "Unidade":'Pç',
+        "Valor": 20,
+    },
+]
+
+
+
+const departamentos = [
+    {
+        "idDep": 1,
+        "Descricao": "Sec. Educação",
+        "Responsavel": "José"
+    },
+    {
+        "idDep": 2,
+        "Descricao": "Sec. Trabalho",
+        "Responsavel": "Menino Feio"
+    },
+    {
+        "idDep": 3,
+        "Descricao": "NAT",
+        "Responsavel": "Joana Like"
+    }
+]
+
+const funcionarios = [
+    {
+        "idFuncionario": 1,
+        "Nome": "Pedro",
+        "Cargo": "Atendente"
+    },
+    {
+        "idFuncionario": 2,
+        "Nome": "Taina",
+        "Cargo": "Supervisora"
+    },
+    {
+        "idFuncionario": 3,
+        "Nome": "Milena",
+        "Cargo": "Diretora"
+    }
 ]
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,27 +1,10 @@
-// document.getElementById('departamento').addEventListener('focus', () => {
-
-//     const inputDepartamento = document.getElementById('departamento');
-//     inputDepartamento.style.backgroundColor='#98FB98'
-// })
-
-
-// document.getElementById('departamento').addEventListener('blur', () => {
-
-//     const inputDepartamento = document.getElementById('departamento');
-//     inputDepartamento.style.backgroundColor='white'
-// })
-
-
 
 function adicionarCorAoFocarInput() {
     const ListInput = document.querySelectorAll('input')
     console.log(ListInput.length);
     console.log(ListInput);
 
-    // Array.prototype.map.call(ListInput, (object)=>{
-    //     object.style.backgroundColor='#98FB98'
-    // });
-
+    
 
     ListInput.forEach((object) => {
         object.addEventListener('focus', () => {
@@ -106,14 +89,14 @@ function exibirDadosTabela(id, produto, estoque, quantidade) {
         }
 
         if (codigo == id && quantidade <= Qtd && quantidade != '') {
-            const stringAux = "<tr id='teste"+ count +"'><td>" + codigo + "</td><td>" + produto + "</td><td>" + Qtd + "</td><td>" + quantidade + "</td><td>R$" + Preco + "</td><td>R$" + Preco * quantidade + "</td><td><button class='BtnRemoverItens' onclick='remover("+ count +", "+ Preco * quantidade +" , "+ codigo +" , "+ quantidade +")'>Remover</button></td></tr>";
+            const stringAux = "<tr id='teste" + count + "'><td>" + codigo + "</td><td>" + produto + "</td><td>" + Qtd + "</td><td>" + quantidade + "</td><td>R$" + Preco + "</td><td>R$" + Preco * quantidade + "</td><td><button class='BtnRemoverItens' onclick='remover(" + count + ", " + Preco * quantidade + " , " + codigo + " , " + quantidade + ")'>Remover</button></td></tr>";
             document.getElementById("tabelaTeste").innerHTML += stringAux;
             count += 1;
 
             Produto.Estoque = Qtd - quantidade;
             document.getElementById("Estoque").value = Produto.Estoque;
 
-            
+
             if (document.getElementById("total").value != '') {
                 document.getElementById("total").value = parseInt(document.getElementById("total").value) + Preco * quantidade;
             } else {
@@ -127,8 +110,8 @@ function exibirDadosTabela(id, produto, estoque, quantidade) {
 
 }
 
-function remover(id, total, codigo, quantidade){
-    document.getElementById("teste"+ id).remove();
+function remover(id, total, codigo, quantidade) {
+    document.getElementById("teste" + id).remove();
     document.getElementById("total").value = parseInt(document.getElementById("total").value) - total;
 
     const produto = produtos.filter((e) => e.idProduto == codigo);
@@ -150,3 +133,107 @@ function adicionarProduto() {
 
     document.getElementById("Quantidade").value = '';
 }
+
+
+
+
+
+
+function validarCampos() {
+    const camposObrigatorios = [
+        'inpNumero',
+        'idDepartamento',
+        'departamento',
+        'dataRequisicao',
+        'idFuncionario',
+        'cargo',
+        'categoriaMotivo',
+        'Motivo',
+        'urgente',
+        'medio',
+        'baixo'
+    ];
+
+    camposObrigatorios.forEach(campoId => {
+        const campo = document.getElementById(campoId);
+        campo.style.backgroundColor = '';
+    });
+
+    const radioPrioridadeDiv = document.querySelector('.radioPrioridade');
+        radioPrioridadeDiv.style.border = '';
+        radioPrioridadeDiv.style.borderRadius = '';
+
+    let camposFaltando = [];
+    camposObrigatorios.forEach(campoId => {
+        const campo = document.getElementById(campoId);
+
+        if (campo.tagName === 'SELECT' && campo.value === '0') {
+            camposFaltando.push(campo);
+        } else if (campo.type === 'radio' && !document.querySelector(`input[name="${campo.name}"]:checked`)) {
+            camposFaltando.push(campo);
+
+            if (campo.name === 'prioridade') {
+                radioPrioridadeDiv.style.border = '2px solid rgba(255, 0, 0, 0.7)';
+                radioPrioridadeDiv.style.borderRadius = '8px';
+            }
+        } else if (!campo.value.trim()) {
+            camposFaltando.push(campo);
+        }
+    });
+
+    if (camposFaltando.length > 0) {
+        camposFaltando.forEach(campo => {
+            campo.style.backgroundColor = 'rgba(255, 0, 0, 0.7)';
+        });
+
+        alert('Preencha todos os campos obrigatórios antes de gravar.');
+    } else {
+        alert('Dados gravados com sucesso!');
+    }
+}
+
+
+document.getElementById('botaoGravar').addEventListener('click', validarCampos);
+
+
+
+
+
+document.getElementById('idDepartamento').addEventListener("keyup",function(){
+    const codigoPesquisado = document.getElementById('idDepartamento').value;
+    let departamentosFiltrados = departamentos.filter((p)=> p.idDep==codigoPesquisado);
+
+    if (departamentosFiltrados.length>0) {
+        document.getElementById('departamento').value=departamentosFiltrados[0].Descricao;
+        
+    }else{
+        document.getElementById('departamento').value="";
+    }
+});
+
+document.getElementById('idFuncionario').addEventListener("keyup",function(){
+    const codigoPesquisado = document.getElementById('idFuncionario').value;
+    let funcionariosFiltrados = funcionarios.filter((p)=> p.idFuncionario==codigoPesquisado);
+
+    if (funcionariosFiltrados.length>0) {
+        document.getElementById('NomeFuncionario').value = funcionariosFiltrados[0].Nome;
+        document.getElementById('cargo').value = funcionariosFiltrados[0].Cargo;
+        
+    }else{
+        document.getElementById('NomeFuncionario').value="";
+    }
+});
+
+
+document.getElementById('CodigoProduto').addEventListener("keyup", function(){
+    const codigoPesquisado = document.getElementById('CodigoProduto').value;
+    let produtosFiltrados = produtos.filter((p)=> p.idProduto==codigoPesquisado);
+
+    if(produtosFiltrados.length !=='undefined'){
+        document.getElementById('DescricaoProduto').value = produtosFiltrados[0].Descricao;
+        document.getElementById('Estoque').value = produtosFiltrados[0].Estoque;
+        document.getElementById('Quantidade').disabled = false
+    }else{
+        document.getElementById('DescricaoProduto').value = "";
+    }
+})
